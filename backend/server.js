9578a83003e0f2173b0e37e1b995b018248b656f
younes bourennane <younesbourennane.yb@gmail.com>
2023-01-22 15:43:04 +0100

    add connection to mongodDb database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+require('dotenv').config()
+
 const app = express()
 
 const PORT = process.env.port || 5000
+
+mongoose.connect(process.env.MONGODB_URL).then(()=> console.log('connected to mongodb')).catch((err) => console.log(err))
 
 app.listen(PORT , () => console.log('listening on ' + PORT))
