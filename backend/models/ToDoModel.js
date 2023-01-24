32212cc5eb5424172ad7683b45896350c03d923f
younes bourennane <younesbourennane.yb@gmail.com>
2023-01-24 20:38:18 +0100

    add relation between User and ToDo Collection

--- backend/models/ToDoModel.js
+++ backend/models/ToDoModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+const User = require("../models/UserModel")
 
 const todoSchema = new mongoose.Schema({
+    owner : {type: mongoose.Schema.Types.ObjectId ,ref: 'User'},
     title : {
         type : String,
         require : true
--- backend/routes/ToDoRoute.js
+++ backend/routes/ToDoRoute.js
@@ -4,29 +4,42 @@ const router = Router()
 const verifyToken = require('../middleware/isLoggedIn')
 
 // get all ToDos
-router.get('/' ,verifyToken,async (req,res) => {
-    const ToDo = await ToDoModel.find()
-    res.send(ToDo)
+router.get('/:id' ,verifyToken, (req,res) => {
+    // console.log(req.params);
+    ToDoModel.find({owner :req.params.id}, (err,ToDo) =>{
+       
+        if (err){
+            console.log(err);
+        }
+        else{
+            // console.log("Result : ", ToDo);
+            res.send(ToDo)
+        }
+        
+    })
+    
 
 })
 
 // Create ToDo
 router.post('/createToDo', verifyToken, (req,res) => {
-    const {title,description,date} = req.body
+    const {owner,title,description,date} = req.body
+    // console.log(req.body)
     const checked = false
-    ToDoModel.create({title,description,date,checked}).then((data)=>{
+    ToDoModel.create({owner,title,description,date,checked}).then((data)=>{
 
-        console.log("created with success")
-        console.log(data)
+        // console.log("created with success")
+        // console.log(data)
         res.send(data)
     }) })
 
 // Update ToDo
 router.post('/updateToDo',verifyToken, (req,res) => {
     const {_id, title, description, date} = req.body
-
+    // console.log("success")
     ToDoModel.findByIdAndUpdate(_id, {title,description,date}).then(()=>{
         res.send("update with success")
+        // console.log("success")
     }).catch((err) => console.log(err))
     
 } )
